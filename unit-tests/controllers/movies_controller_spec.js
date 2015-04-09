b8f244c614cbe1b17ec672098a48348c9ee9a0ed
aghosh47 <aghosh@kreeti.com>
2015-04-09 21:11:41 +0530

    check changes

--- unit-tests/controllers/movies_controller_spec.js
+++ unit-tests/controllers/movies_controller_spec.js
@@ -1,27 +1,45 @@
-describe('MoviesCtrl', function() {
-  var $rootScope
-  , $controller
-  , services
-  ;
+describe('Controllers', function() {
+  /** initiate the module.
+      it in turn calls the myAppServices and myAppControllers required for testing the controller */
 
-  beforeEach( function() {
-    module('myApp');
-  });
+  beforeEach(module('myApp'));
 
-  beforeEach(function() {
-    module('myAppServices');
-    inject(function($injector) {
-      services    = $injector.get('myAppServices')
-      $rootScope  = $injector.get('$rootScope');
-      $controller = $injector.get('$controller')
+  describe("MoviesCtrl", function() {
+    var scope, httpBackend;
+    /** prepare the mock data */
+    var config_data = { images: { base_url: "http://tmdb.com/t/p", backdrop_sizes: ["w300", "w500"]}},
+        movie_data = {results: [{name: "Abc"}, {name: "Def"}]};
+
+    beforeEach(inject(function($httpBackend, $rootScope, $controller) {
+      httpBackend = $httpBackend;
+      scope = $rootScope.$new();
+
+      /** mock the http request for the configuration data */
+
+      httpBackend.
+        expectGET("https://api.themoviedb.org/3/configuration?api_key=2e329c92227ed8be07944ae447c9426f").
+        respond(config_data);
+
+      /** mock the http request to get the list of movies */
+
+      httpBackend.
+        expectGET("https://api.themoviedb.org/3/movie/top_rated?api_key=2e329c92227ed8be07944ae447c9426f").
+        respond(movie_data);
+
+      /** inject the controller */
+      $controller('MoviesCtrl', { $scope: scope });
+    }));
+
+    it("should return the image object with url and backdrop size", function() {
+      httpBackend.flush();
+      expect(scope.image.backdrop_size).toEqual("w300");
+    });
+
+
+    it("should return a list of movies", function() {
+      httpBackend.flush();
+      expect(scope.movies.length).toEqual(2);
     });
   });
 
-  describe("Intialization", function() {
-    it("should initialize the api url", function() {
-      var $scope = $rootScope.new();
-      var controller = $controller('MoviesCtrl', { $rootScope: $rootScope, $scope: $scope, myAppServices: services });
-      expect($rootScope.api.url).toEqual("https://api.themoviedb.org/3/")
-    });
-  });
 });
